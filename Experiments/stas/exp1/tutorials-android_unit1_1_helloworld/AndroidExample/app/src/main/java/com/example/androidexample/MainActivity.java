88d25a7f00d758e7594efe2c0d956f2c19851e2d
Stas Bronevskiy <sbron@iastate.edu>
2025-02-06 16:14:51 -0600

    Update exp1 dark mode

--- Experiments/stas/exp1/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/stas/exp1/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
+    private Button themeButton;
+
+    private int nightMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);             // link to Main activity XML
 
         /* initialize UI elements */
-        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
+        messageText = findViewById(R.id.mainText);      // link to message textview in the Main activity XML
         messageText.setText("Hello World");
+
+        themeButton = findViewById(R.id.themeButton);
+
+        // init shared prefs
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        // set night mode state (default - system)
+        nightMode = sharedPreferences.getInt("nightMode", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        AppCompatDelegate.setDefaultNightMode(nightMode);
+
+        // change theme on click
+        themeButton.setOnClickListener((View v) -> {
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_NO) {
+                nightMode = AppCompatDelegate.MODE_NIGHT_YES;
+            }
+            else {
+                nightMode = AppCompatDelegate.MODE_NIGHT_NO;
+            }
+            editor.putInt("nightMode", nightMode);
+            editor.apply();
+            AppCompatDelegate.setDefaultNightMode(nightMode);
+        });
+
+
     }
 }
