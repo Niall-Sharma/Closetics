206a4dae991466671fdf1ebfcfbe39e50903ce2b
ashten <afinders@iastate.edu>
2025-02-06 18:48:38 -0600

    Final changes to exp1

--- Experiments/ashten/exp1/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/ashten/exp1/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private Button button1;
     private Button button2;
     private Button button3;
+    private Button button4;
+    private Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         //Third Button
         button3 = findViewById(R.id.thirdButton);
         button3.setText("Press Here!");
+
 
 
 
