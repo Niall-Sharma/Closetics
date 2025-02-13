c62109d54fe2f79b08c881eefea40831edc3a24f
Niall Sharma <nialls@iastate.edu>
2025-02-12 18:03:05 -0600

    added very basic user class

---
+++ Backend/Users/User.java
@@ -0,0 +1,51 @@
+package Backend.Users;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String emailId;
+
+
+    public User(String name, String emailId) {
+        this.name = name;
+        this.emailId = emailId;
+    }
+
+    public User() {
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmailId(){
+        return emailId;
+    }
+
+    public void setEmailId(String emailId){
+        this.emailId = emailId;
+    }
+
+
+}
