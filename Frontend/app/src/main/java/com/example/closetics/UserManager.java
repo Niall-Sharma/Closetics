8ca5e9a081175f14828584b406af279658612136
ashten <afinders@iastate.edu>
2025-02-19 15:35:04 -0600

    UserManager method stubs and VolleySingleton class

---
+++ Frontend/app/src/main/java/com/example/closetics/UserManager.java
@@ -0,0 +1,13 @@
+package com.example.closetics;
+
+public class UserManager {
+
+
+    //Example method stubs
+    public static String getUsername(){
+        return null;
+    }
+    public static String getPassword(){
+        return null;
+    }
+}
