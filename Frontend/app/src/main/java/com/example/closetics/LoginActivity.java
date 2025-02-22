e81d671e741c634950251e4093e1749c521b5896
ashten <afinders@iastate.edu>
2025-02-22 17:39:09 -0600

    Push request for logging in when pressing login button

--- Frontend/app/src/main/java/com/example/closetics/LoginActivity.java
+++ Frontend/app/src/main/java/com/example/closetics/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.example.closetics;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -15,16 +21,25 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
+//10.0.2.2
 public class LoginActivity extends AppCompatActivity {
 
     private EditText usernameEditText;  // define username edittext variable
     private EditText passwordEditText;  // define password edittext variable
     private Button loginButton;         // define login button variable
     private Button signupButton;        // define signup button variable
+
+    private int userId;
     //Postman Mock Server
-    private static final String URL = "https://803bd0f1-d0c7-422a-a0c5-06cdf909e51f.mock.pstmn.io";
+    //private static final String URL = "https://803bd0f1-d0c7-422a-a0c5-06cdf909e51f.mock.pstmn.io";
+   
+    private static final String URL = "https://jsonplaceholder.typicode.com/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +56,42 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(v -> {
             String username = usernameEditText.getText().toString();
             String password = passwordEditText.getText().toString();
+            //Login request to the backend API
+            UserManager.loginRequest(getApplicationContext(), username, password, URL,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            //Response is a JSON object from the backend end
+                            try {
+                                String token = response.getString("token");//This grabs the string value of JSON header
+                                UserManager.saveLoginToken(getApplicationContext(), token);
+                                //Username???
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            if (error.networkResponse != null){
+                                int statusCode = error.networkResponse.statusCode;
+                                String errorBody = null;
+
+                                //HttpStatus.UNAUTHORIZED
+                                if (statusCode == HttpURLConnection.HTTP_UNAUTHORIZED){
+                                    try{
+                                        errorBody = new String (error.networkResponse.data, "UTF-8");
+                                        Log.d("Error", "401 Unauthorized");
+
+                                    }catch(UnsupportedEncodingException e){
+                                        e.printStackTrace();
+                                    }
+                                }
+                                //More
+                            }
+                        }
+                    });
 
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             intent.putExtra("USERNAME", username);
@@ -53,49 +104,13 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);
+
             }
+
         });
     }
 
-    //This uses a post request to send the user's input for username and password so that
-    //authentication is handled in the backend versus the frontend (more secure than get requests
-    // to the frontend)
-    private void loginRequest(){
-
-        //Create the json object of the login data (username and password)
-        JSONObject loginData = new JSONObject();
-
-        //Use try catch blocks when creating JSON objects
-        try{
-            loginData.put();
-            //Use proper headers for the backend as string,
-            // int will be the actual data (username/password)
-            loginData.put();
-        }
-        catch(JSONException e){
-            e.printStackTrace();
-        }
-
-
-        //The push request
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.Post,
-                URL,
-                loginData,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-        //Add request to the volley singleton request queue
 
 
 
-
-    }
 }
--- Frontend/app/src/main/java/com/example/closetics/UserManager.java
+++ Frontend/app/src/main/java/com/example/closetics/UserManager.java
@@ -1,13 +1,68 @@
 package com.example.closetics;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+
 public class UserManager {
 
+    private static final String SHARED_PREFERENCES_FILE_NAME = "CloseticsPreferences";
 
-    //Example method stubs
-    public static String getUsername(){
-        return null;
+
+    //This uses a post request to send the user's input for username and password so that
+    //authentication is handled in the backend versus the frontend (more secure than get requests
+    // to the frontend)
+    public static void loginRequest(Context context, String username, String password, String URL,
+                                     Response.Listener<JSONObject> responseListener,
+                                     Response.ErrorListener errorListener) {
+
+        //Create the json object of the login data (username and password)
+        JSONObject loginData = new JSONObject();
+
+        //Use try catch blocks when creating JSON objects
+        try {
+            loginData.put("username", username);
+            loginData.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        //The post request
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                URL,
+                loginData, responseListener, errorListener);
+        //Add request to the volley singleton request queue
+        VolleySingleton.getInstance(context).addToRequestQueue(request);
     }
-    public static String getPassword(){
-        return null;
+    //This method stores the login token in sharedPreferences (standard)
+    //Shared preferences used when a value needs to persist across app sessions
+
+    //CloseticsPreferences is now the name of the shared preference file!
+    public static void saveLoginToken(Context context, String token) {
+        //Access the shared preferences file make it private to this app
+        SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("logInToken", token);
+        editor.apply();
+
     }
+
+
+
 }
+
+
+
