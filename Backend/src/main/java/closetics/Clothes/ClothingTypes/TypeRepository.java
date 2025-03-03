7618c85b1b692bf8336bc58ababfad2410bd4524
Niall Sharma <Niall.k.sharma@gmail.com>
2025-03-03 13:36:04 -0600

    Added special queries to type and special type repository

--- Backend/src/main/java/closetics/Clothes/ClothingTypes/TypeRepository.java
+++ Backend/src/main/java/closetics/Clothes/ClothingTypes/TypeRepository.java
@@ -1,11 +1,16 @@
 package closetics.Clothes.ClothingTypes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface TypeRepository extends JpaRepository<ClothingType, Long> {
-
+  @Query(value = "SELECT * FROM types WHERE type_name=?",nativeQuery = true)
+  ClothingType findByType(String typeName);
+  
+  @Query(value = "SELECT * FROM types WHERE id = ?", nativeQuery = true)
+  ClothingType findByTypeId(int typeId);
 
 }
