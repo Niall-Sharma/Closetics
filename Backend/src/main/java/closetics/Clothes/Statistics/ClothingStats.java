7629f9683672d6f54c7564ab8c3c8ca0984d7a83
Hayworth, Brayden N <braydenh@iastate.edu>
2025-03-26 17:31:48 -0500

    Added element collections for the dates worn arrays so a join table will get made for both

--- Backend/src/main/java/closetics/Clothes/Statistics/ClothingStats.java
+++ Backend/src/main/java/closetics/Clothes/Statistics/ClothingStats.java
@@ -7,81 +7,90 @@ import jakarta.persistence.*;
 
 @Entity(name = "clothing_stats_table")
 public class ClothingStats {
-  ArrayList<Date> dateWorn;
-  long timesWorn;
-  float highTemp;
-  float lowTemp;
-  long numberOfOutfitsIn;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long clothingStatsId;
+
+    @OneToOne
+    @JoinColumn(name = "clothing_id", nullable = false)
+    long clothingId;
+
+    @ElementCollection
+    @CollectionTable(name = "clothing_item_days_worn", joinColumns = @JoinColumn(name = "clothing_stats_id"))
+    @Column(name = "clothing_stats_id")
+    ArrayList<Date> datesWorn;
+
+    long timesWorn;
+    float highTemp;
+    float lowTemp;
+    long numberOfOutfitsIn;
 
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  long clothingStatsId;
+    public ClothingStats(long timesWorn, float highTemp, float lowTemp, long numberOfOutfitsIn) {
+        this.timesWorn = timesWorn;
+        this.datesWorn = new ArrayList<>();
+        this.highTemp = highTemp;
+        this.lowTemp = lowTemp;
+        this.numberOfOutfitsIn = numberOfOutfitsIn;
 
-  public ClothingStats(long timesWorn, float highTemp, float lowTemp, long numberOfOutfitsIn){
-    this.timesWorn = timesWorn;
-    this.dateWorn = new ArrayList<>();
-    this.highTemp = highTemp;
-    this.lowTemp = lowTemp;
-    this.numberOfOutfitsIn =numberOfOutfitsIn;
+    }
 
-  }
-  
-  public ClothingStats(){
-    dateWorn = new ArrayList<>();
-    timesWorn = 0;
-    highTemp = 0;
-    lowTemp = 0;
-    numberOfOutfitsIn = 0;
-  }
+    public ClothingStats() {
+        datesWorn = new ArrayList<>();
+        timesWorn = 0;
+        highTemp = 0;
+        lowTemp = 0;
+        numberOfOutfitsIn = 0;
+    }
 
-  public float getLowTemp(){
-    return lowTemp;
-  }
+    public float getLowTemp() {
+        return lowTemp;
+    }
 
-  public void setLowTemp(float temp){
-    lowTemp = temp;
-  }
+    public void setLowTemp(float temp) {
+        lowTemp = temp;
+    }
 
-  public float getHighTemp(){
-    return highTemp;
-  }
+    public float getHighTemp() {
+        return highTemp;
+    }
 
-  public void setHighTemp(float temp){
-    highTemp = temp;
-  }
+    public void setHighTemp(float temp) {
+        highTemp = temp;
+    }
 
-  public long getTimesWorn(){
-    return timesWorn;
-  }
+    public long getTimesWorn() {
+        return timesWorn;
+    }
 
-  public void setTimesWorn(long timesWorn){
-    this.timesWorn = timesWorn;
-  }
+    public void setTimesWorn(long timesWorn) {
+        this.timesWorn = timesWorn;
+    }
 
-  public void incrementTimesWorn(){
-    timesWorn+=1;
-  }
+    public void incrementTimesWorn() {
+        timesWorn += 1;
+    }
 
-  public void decrementTimesWorn(){
-    timesWorn-=1;
-  }
+    public void decrementTimesWorn() {
+        timesWorn -= 1;
+    }
 
-  public void addLastWorn(Date lastWorn){
-    this.dateWorn.add(lastWorn);
-  }
+    public void addLastWorn(Date lastWorn) {
+        this.datesWorn.add(lastWorn);
+    }
 
-  public ArrayList<Date> getLastWorn(){
-    return dateWorn;
-  }
+    public ArrayList<Date> getLastWorn() {
+        return datesWorn;
+    }
 
-  public long numberOfOutfitsIn() {
-    return numberOfOutfitsIn;
-  }
+    public long numberOfOutfitsIn() {
+        return numberOfOutfitsIn;
+    }
 
-  public void setNumberOfOutfitsIn(long numberOfOutfitsIn) {
-    this.numberOfOutfitsIn = numberOfOutfitsIn;
-  }
+    public void setNumberOfOutfitsIn(long numberOfOutfitsIn) {
+        this.numberOfOutfitsIn = numberOfOutfitsIn;
+    }
 }
 
 
