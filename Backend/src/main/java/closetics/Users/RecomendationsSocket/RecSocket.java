f81068574cd1408e8c147545bd2e96d132ef46a2
Niall Sharma <Niall.k.sharma@gmail.com>
2025-04-01 17:33:19 -0500

    finished prototype version

--- Backend/src/main/java/closetics/Users/RecomendationsSocket/RecSocket.java
+++ Backend/src/main/java/closetics/Users/RecomendationsSocket/RecSocket.java
@@ -2,6 +2,7 @@
 package closetics.Users.RecomendationsSocket;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
@@ -20,16 +21,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import closetics.Outfits.Outfit;
+import closetics.Outfits.OutfitRepository;
 import closetics.Users.UserProfile.UserProfile;
 import closetics.Users.UserProfile.UserProfileRepository;
 
 @Controller      // this is needed for this to be an endpoint to springboot
-@ServerEndpoint(value = "/chat/{username}")  // this is Websocket url
+@ServerEndpoint(value = "/recomendation/{uid}")  // this is Websocket url
 public class RecSocket {
 
   // cannot autowire static directly (instead we do it by the below
   // method
 	private static UserProfileRepository UserProfileRepository; 
+  private static OutfitRepository OutfitRepository;
 
 	/*
    * Grabs the MessageRepository singleton from the Spring Application
@@ -39,13 +43,19 @@ public class RecSocket {
    * easiest.
 	 */
 	@Autowired
-	public void setMessageRepository(UserProfileRepository repo) {
+	public void setUserProfileRepository(UserProfileRepository repo) {
 		UserProfileRepository = repo;  // we are setting the static variable
 	}
+
+  @Autowired
+  public void setOutfitRepository(OutfitRepository repo){
+    OutfitRepository = repo;
+  }
 
 	// Store all socket session and their corresponding username.
 	private static Map<Session, Long> sessionUsernameMap = new Hashtable<>();
 	private static Map<Long, Session> uidSessionMap = new Hashtable<>();
+  private static Map<Long, List<Outfit>> followingOutfitMap = new Hashtable<>();
 
 	private final Logger logger = LoggerFactory.getLogger(RecSocket.class);
 
@@ -58,6 +68,13 @@ public class RecSocket {
     // store connecting user information
 		sessionUsernameMap.put(session, UID);
 		uidSessionMap.put(UID, session);
+    UserProfile uProfile = UserProfileRepository.findById(UID);
+    List<UserProfile> following = uProfile.GetFollowing();
+    List<Outfit> followingOutfits = new ArrayList<>();
+    for(int i = 0; i < following.size();i++){
+      followingOutfits.addAll(following.get(i).GetOutfits());
+    }
+    followingOutfitMap.put(UID, followingOutfits);
 
 		
 	}
@@ -80,6 +97,7 @@ public class RecSocket {
 		long UID = sessionUsernameMap.get(session);
 		sessionUsernameMap.remove(session);
 		uidSessionMap.remove(UID);
+    followingOutfitMap.remove(UID);
 
 	}
 
@@ -92,23 +110,21 @@ public class RecSocket {
 
 
 	private void sendRec(long UID) {
+    int recSize = 30;
+    List<Outfit> RecList = new ArrayList<>();
 		try {
-      UserProfile user = UserProfileRepository.findById(UID);
-      
+      for(int i = 0; i < recSize; i++){
       int randomChoice = (int)((Math.random()*10)+1);
-      UserProfile recomendation = new UserProfile(); 
       if(randomChoice > 5){
-        //Pick from following
-        int randomFollower = (int)((Math.random()*user.GetFollowing().size()));
-        recomendation = user.GetFollowing().get(randomFollower);
+        int randomOutfitInteger = (int)((Math.random()*followingOutfitMap.get(UID).size()));
+        RecList.add(followingOutfitMap.get(UID).remove(randomOutfitInteger)); 
       }else{
-        int randomUser = (int)(Math.random()*UserProfileRepository.count());
+
+        Long randomOutfit = Math.round(Math.random()*OutfitRepository.count());
+        RecList.add(OutfitRepository.findById(randomOutfit).get());
       }
-
-
-
-
-			uidSessionMap.get(UID).getBasicRemote().sendObject(recomendation);
+      }
+        uidSessionMap.get(UID).getBasicRemote().sendObject(RecList);
 		} 
     catch (IOException e) {
 			logger.info("Exception: " + e.getMessage().toString());
