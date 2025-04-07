9cbb8d90b91576c8bb165b5d3a9d786902031db6
ashten <afinders@iastate.edu>
2025-04-06 21:52:48 -0500

    Web socket connection established, endpoint change on filter change, update every 10 seconds

--- Frontend/app/src/main/java/com/example/closetics/dashboard/LeaderboardActivity.java
+++ Frontend/app/src/main/java/com/example/closetics/dashboard/LeaderboardActivity.java
@@ -2,6 +2,7 @@ package com.example.closetics.dashboard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,6 +23,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.closetics.MainActivity;
 import com.example.closetics.R;
 import com.example.closetics.UserManager;
 import com.example.closetics.clothes.ClothesActivity;
@@ -30,9 +32,16 @@ import com.example.closetics.clothes.ClothingItem;
 import com.example.closetics.clothes.EditClothesActivity;
 
 import java.net.URISyntaxException;
+import java.nio.channels.AlreadyConnectedException;
 import java.util.ArrayList;
 
 public class LeaderboardActivity extends AppCompatActivity {
+
+    /*
+     Continually update the leaderboard with Runnable and Handler
+     */
+    private Runnable continuousMessaging;
+    private Handler handler;
 
     /*
     Websocket client
@@ -43,13 +52,19 @@ public class LeaderboardActivity extends AppCompatActivity {
     private Spinner leaderboardFilter;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
-    private ClothesByTypeAdapter adapter;
+    private LeaderboardRecyclerViewAdapter adapter;
     private String chosenFilter;
+    private URI uri;
+    private boolean isConnected  = false;
+
+
+    private int spinnerPosition; //Default to first spinner item
     private TextView categoryValue;
     private final String[] spinnerItems = new String[]
             {"Most Expensive Clothing", "Most Expensive Outfits", "Largest Closet" };
 
-    private final String WEB_SOCKET_URL = "ws://localhost:8080/leaderboard/";
+    private final String WEB_SOCKET_URL = MainActivity.SERVER_WS_URL +"/leaderboard/"; // + {username}/{type}
+
 
 
     @Override
@@ -70,17 +85,6 @@ public class LeaderboardActivity extends AppCompatActivity {
         categoryValue = findViewById(R.id.category);
 
         /*
-        Creating the websocket client/handshake
-         */
-        setWebSocketClient();
-        try {
-            webSocketClient.connect();
-        } catch (Exception e) {
-            Log.e("WebSocket", "Connection failed: " + e.getMessage());
-        }
-
-
-        /*
         Note this will have one more item
          */
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spinnerItems);
@@ -91,18 +95,39 @@ public class LeaderboardActivity extends AppCompatActivity {
             //Store chosen question
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinnerPosition = position + 1;
                 chosenFilter = (String) parent.getItemAtPosition(position);
                 categoryValue.setText(chosenFilter);
+                if (isConnected){
+                    webSocketClient.close();
+                }
+                connectWebSocketClient();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                chosenFilter= (String)parent.getItemAtPosition(0);
-                categoryValue.setText(chosenFilter);
-
             }
         });
-        
+
+        handler = new Handler();
+        continuousMessaging = new Runnable() {
+            @Override
+            public void run() {
+                //Send a message to websocket
+                String message = "PLEASE_SEND_ME_THE_LEADERBOARDDDDDD";
+                if (webSocketClient!=null) {
+                    webSocketClient.send(message);
+                }
+
+                //Delay of every 10 seconds
+                handler.postDelayed(this, 10000);
+
+            }
+        };
+
+        handler.post(continuousMessaging);
+
+
 
 
 
@@ -146,67 +171,96 @@ public class LeaderboardActivity extends AppCompatActivity {
          */
 
 
+    }
+
+    private void setWebSocketClient(URI uri){
+
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+                Log.d("Websocket", "Connected to Websocket server!");
+                //Loading UI?
+                //Connected UI?
 
 
+            }
+
+            @Override
+            public void onMessage(String message) {
+                Log.d("Websocket", "Message received: " + message);
+                isConnected = true;
+
+                if (message.equals("Username already exists")){
+                    throw new AlreadyConnectedException();
+                    //print an error message
+                    //Do more?
+                }
+
+                //Grab the JSONArray
+                try {
+                    JSONArray leaderboardArray = new JSONArray(message);
+                    //Parse the array and grab the fields needed for the recycler view
+                    for (int i = 0; i < leaderboardArray.length(); i++){
+                        Log.d("Object", leaderboardArray.get(i).toString());
+                    }
+
+                } catch (JSONException e) {
+                    Log.e("JSONArray Error", e.toString());
+                }
+
+                }
+
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+                Log.d("Websocket", "Closed with reason: " + reason);
+                //Handle UI updates or reconnection attempts if needed
+                isConnected = false;
+
+
+            }
+
+            @Override
+            public void onError(Exception ex) {
+                Log.e("Websocket", "Error: " + ex.getMessage());
+                //Show error dialog or retry connection
+
+            }
+        };
 
     }
-    private void setWebSocketClient(){
+    private void setUri() {
         try {
-
-            URI uri = new URI(WEB_SOCKET_URL + UserManager.getUsername(this));
-
-            webSocketClient = new WebSocketClient(uri) {
-                @Override
-                public void onOpen(ServerHandshake handshakedata) {
-                    Log.d("Websocket", "Connected to Websocket server!");
-                    //Loading UI?
-                    //Connected UI?
-
-
-                }
-
-                @Override
-                public void onMessage(String message) {
-                    Log.d("Websocket", "Message received: " + message);
-
-                    if (message.equals("Username already exists")){
-                        //print an error message
-
-                    }
-                    else{
-                        //Grab the JSONArray
-                        try {
-                            JSONArray leaderboardArray = new JSONArray(message);
-                            //Parse the array and grab the fields needed for the recycler view
-                            for (int i = 0; i < leaderboardArray.length(); i++){
-                                Log.d("Object", leaderboardArray.get(i).toString());
-                            }
-
-                        } catch (JSONException e) {
-                            Log.e("JSONArray Error", e.toString());
-                        }
-
-                    }
-                }
-
-                @Override
-                public void onClose(int code, String reason, boolean remote) {
-                    Log.d("Websocket", "Closed with reason: " + reason);
-                    //Handle UI updates or reconnection attempts if needed
-
-
-                }
-
-                @Override
-                public void onError(Exception ex) {
-                    Log.e("Websocket", "Error: " + ex.getMessage());
-                    //Show error dialog or retry connection
-
-                }
-            };
-        } catch (URISyntaxException e) {
+            uri = new URI(WEB_SOCKET_URL + UserManager.getUsername(this) + "/" + spinnerPosition);
+        }catch (URISyntaxException e) {
             //throw new RuntimeException(e);
             Log.e("error", e.toString());
+        }
+    }
+
+    private void connectWebSocketClient(){
+        setUri();
+        setWebSocketClient(uri);
+        try {
+            webSocketClient.connect();
+        } catch (Exception e) {
+            Log.e("WebSocket", "Connection failed: " + e.getMessage());
+        }
+
+    }
+
+
+    /*
+    When the activity stops, stop sending messages and disconnect websocket
+     */
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if (handler!=null){
+            handler.removeCallbacks(continuousMessaging);
+        }
+        if (webSocketClient!=null){
+            webSocketClient.close();
         }
     }
 
