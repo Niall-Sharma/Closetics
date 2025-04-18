f2a1228309357742d5436cf802099b396345a21a
Niall Sharma <nialls@iastate.edu>
2025-04-17 22:28:38 -0500

    whitespace

--- Backend/src/main/java/closetics/MainApplication.java
+++ Backend/src/main/java/closetics/MainApplication.java
@@ -3,6 +3,7 @@ package closetics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MainApplication {
 
